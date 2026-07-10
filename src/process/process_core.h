84c3bfcca48cf910d48f50e01dd7cbe1a39cadec
Xetch <poopyking482@gmail.com>
2026-07-10 18:37:33 +0800

    Create process/process_core.h

---
+++ src/process/process_core.h
@@ -0,0 +1,13 @@
+#ifndef PROCESS_CORE_H
+#define PROCESS_CORE_H
+
+void shell_fork(void);
+void shell_kill(const char *pid_str);
+void shell_call(char *tokens[], int token_count);
+
+#endif
+// did you just PROCESS_CORE?
+// boi:yessirski
+// BOI WHY DID YOU LIE TO ME
+// homework nah homework nah
+// I did it at school
