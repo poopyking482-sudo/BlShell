6ef677e3f972dec382261095a27fe117042eeadf
Xetch <poopyking482@gmail.com>
2026-07-12 13:06:02 +0800

    Update blterm.c

--- src/process/blterm.c
+++ src/process/blterm.c
@@ -1,6 +1,6 @@
 #include "process_core.h"
 
 void shell_blterm(const char *pid_str) {
-    // Directly forwards the argument straight to your main term logic
+    // directly forwards the argument straight to main term logic
     shell_term(pid_str);
 }
